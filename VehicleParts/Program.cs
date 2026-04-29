a2f5cc8893f081e508e5e6463b1bb28b6b58a1c2
Kriti Tripathi <tripathikriti483@gmail.com>
2026-04-29 10:15:40 +0545

    removed mock email

--- VehicleParts/Program.cs
+++ VehicleParts/Program.cs
@@ -2,10 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using VehicleParts.Application.Middlewares;
 using VehicleParts.Infrastructure;
 using VehicleParts.Infrastructure.Data;
-using VehicleParts.Application.Interfaces;
-using VehicleParts.Application.Services.Customer;
-using VehicleParts.Application.Services.Sales;
-using VehicleParts.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,32 +11,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
 builder.Services.AddInfrastructure(
-    builder.Configuration);
+    builder.Configuration
+);
 
-// Register Application Services (AFTER Infrastructure)
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddScoped<ICustomerProfileService, CustomerProfileService>();
-builder.Services.AddScoped<ICustomerBookingService, CustomerBookingService>();
-builder.Services.AddScoped<ICustomerRequestService, CustomerRequestService>();
-builder.Services.AddScoped<ICustomerReviewService, CustomerReviewService>();
-builder.Services.AddScoped<IStaffCustomerHistoryService, StaffCustomerHistoryService>();
-builder.Services.AddScoped<ISalesService, SalesService>();
-builder.Services.AddScoped<IVendorService, VendorService>();
-builder.Services.AddScoped<IPartService, PartService>();
-builder.Services.AddScoped<IPurchaseService, PurchaseService>();
-builder.Services.AddScoped<IEmailService, MockEmailService>();
-
-// CORS for frontend
 // CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
@@ -48,14 +30,11 @@ var app = builder.Build();
 // Database + Seeder
 using (var scope = app.Services.CreateScope())
 {
-    var db =
-        scope.ServiceProvider
-            .GetRequiredService<AppDbContext>();
+    var db = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
 
     await db.Database.MigrateAsync();
-
-    await DbSeeder.SeedAsync(
-        scope.ServiceProvider);
+    await DbSeeder.SeedAsync(scope.ServiceProvider);
 }
 
 // OpenAPI
@@ -68,7 +47,6 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
-
 app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
