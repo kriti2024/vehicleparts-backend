7340ddd780903485dab5df2b4c1a4e26f8035a8c
Kriti Tripathi <tripathikriti483@gmail.com>
2026-05-17 13:36:43 +0545

    migrations

---
+++ VehicleParts.Infrastructure/Migrations/20260513145418_AddCreditReminder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleParts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreditReminder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPaymentDate",
+                table: "Customers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PendingCredit",
+                table: "Customers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPaymentDate",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PendingCredit",
+                table: "Customers");
+        }
+    }
+}
